98658de5b36c26114a9dbe510ebd7edd8f41eef3
Wondwosen <revatoel@gmail.com>
2024-03-11 14:07:41 +0300

    adding to the file

--- data_types.py
+++ data_types.py
@@ -1,10 +1,23 @@
 # Sting data types
-
 # literal assignments
-
 first = 'Wondwosen'
 last = 'Asegid'
 
 # strings can be concatenated
-
 print(first + ' ' + last)
+
+# checking the type of  data
+
+x = 47
+
+# using the type function to check the data
+print(type(x))
+
+# using isinstance
+
+d_type = isinstance(first, int)  # returns false cuz iss not of the int type
+print(d_type)
+
+
+
+
