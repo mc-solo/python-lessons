07de4a79faf7a5d15728bde9aa47718d9dc2b366
Wondwosen <revatoel@gmail.com>
2024-03-27 02:16:12 +0300

    basic if-elsde statement

---
+++ control_flow.py
@@ -0,0 +1,11 @@
+# A basic way of decision making
+
+# if = Do some code only IF some conditions are met
+#   Else do something else
+
+age = int(input("Please, enter your age: "))
+
+if age >= 18:
+    print("You're signed up!")
+else:
+    print("grow tf up!!!")
--- data_types.py
+++ data_types.py
@@ -4,8 +4,8 @@ import math
 # Sting data types
 # literal assignments
 
-first = 'Wondwosen'
-last = 'Asegid'
+first = "Wondwosen"
+last = "Asegid"
 
 # checking the type of  data
 # x = 47
@@ -20,11 +20,11 @@ print(d_type)
 
 # using a constructor function to define a var
 
-goal = str('to become a rich, high value man')
+goal = str("to become a rich, high value man")
 print(goal)
 
 # concatenation in python
-print(first + ' ' + last)
+print(first + " " + last)
 
 # In python, casting refers to the process of converting one data type to another.
 num_str = 10
@@ -33,39 +33,41 @@ num_int = int(num_str)
 print(num_int)
 print(isinstance(num_int, int))
 
-bool_str = 'True'
+bool_str = "True"
 bool_val = bool(bool_str)
 
 print(type(bool_val))
 
 # Escaping special characters
-sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+sentence = "I'm back at work!\tHey!\n\nWhere's this at\\located?"
 # print(sentence)
 
 # String Methods
 #
-multiline = '''
+multiline = """
 This is a multiline sentence.
 
             I am so lucky to have this in my early ages.
 This is the end of multiline sentence.
 
-'''
+"""
 
 print(multiline)
-print(multiline.title())  # converts to a proper case - capitalizing the 1st letter of each word
-print(multiline.replace('lucky', 'thankful'))  # find and replace
+print(
+    multiline.title()
+)  # converts to a proper case - capitalizing the 1st letter of each word
+print(multiline.replace("lucky", "thankful"))  # find and replace
 
 # len() - this checks the length of the string
 print(len(multiline))
-multiline += '              '
-multiline = '               ' + multiline
+multiline += "              "
+multiline = "               " + multiline
 print(len(multiline))
 
 # building a new menu
 
-title = 'menu'.upper()
-newTitle = title.center(47, '*')
+title = "menu".upper()
+newTitle = title.center(47, "*")
 #
 # print(newTitle)
 # print('coffee'.ljust(25, '.') + '$1'.rjust(4,' ' ))
@@ -79,8 +81,8 @@ print(first[-1])  # the last letter of the string
 print(first[1:])  # starts from the second and goes up to the last letter of the string
 
 # Some methods return boolean data
-print(first.startswith('W'))
-print(first.endswith('W'))  # This shi* is case-sensitive
+print(first.startswith("W"))
+print(first.endswith("W"))  # This shi* is case-sensitive
 
 # Boolean data type
 myValue = True
@@ -115,10 +117,9 @@ print(math.sqrt(64))
 
 
 # Casting a string to a number
-zipcode = '10001'
+zipcode = "10001"
 zip_value = int(zipcode)
 print(type(zip_value))
 
 # Error if you attempt to cast incorrect data
 # zip_value = int('Mc_solo')
-
