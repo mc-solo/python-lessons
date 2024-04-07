9246d71874df37d1c13949e5bcb68f80e8a0c444
Wondwosen <revatoel@gmail.com>
2024-04-07 12:50:55 +0300

    done with the basics i guess

--- functions.py
+++ functions.py
@@ -4,5 +4,16 @@ def add_numbers(a, b):
 
 print("done with the funciton")
 
+
 summation = add_numbers(4, 47)
 print(summation)
+
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
+
+
+temp_celsius = 30
+
+temp_f = celsius_to_fahrenheit(temp_celsius)
+print(temp_f)
