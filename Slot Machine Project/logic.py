aa0f3cd6d58801bce89e198b088cb79912836a50
Wondwosen <revatoel@gmail.com>
2024-04-09 01:38:37 +0300

    fix get lines

--- Slot Machine Project/logic.py
+++ Slot Machine Project/logic.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3  # global const for the max number of lines
+MAX_BET = 10
+MIN_BET = 2
 
 
 # Get amount from the user
@@ -27,16 +29,35 @@ def get_number_of_lines():
             lines = int(lines)
             if 1 <= lines <= MAX_LINES:
                 break
+            else:
+                print("Enter a valid num between 1 and 3")
         else:
-            print("Please, enter a numbert")
+            print("Input is not a number")
     return lines
 
 
+# get the number of bets
+def get_bet():
+    while True:
+        bet = input("What would you like to bet?")
+        if bet.isdigit:
+            bet = int(bet)
+            if MIN_BET <= bet <= MAX_BET:
+                break
+            else:
+                print("Value must be between min and max bet ")
+        else:
+            print("Please, enter a valid number of bets")
+    return bet
+
+
 # Program should start from the main function
-def main():
-    balance = deposit()
-    lines = get_number_of_lines()
-    print(balance, lines)
+# def main():
+#     balance = deposit()
+#     lines = get_number_of_lines()
+#     bet = get_bet()
+#     print(balance, lines)
 
 
-main()
+# main()
+get_number_of_lines()
