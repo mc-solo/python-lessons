e4be187ade17a4fc5e5116e7553b2715863c97a4
Wondwosen <revatoel@gmail.com>
2024-04-11 01:55:50 +0300

    keys, values and items

--- Basics/new_shits_for_me.py
+++ Basics/new_shits_for_me.py
@@ -1,4 +1,13 @@
-symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
+# symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
 
-for letters, nums in symbol_count.items():
-    print(letters, nums)
+# for letters, nums in symbol_count.items():
+#     print(letters, nums)
+
+
+# dictionary to list conversion
+
+my_dict = {"a": 1, "b": 2, "c": 3}
+keys_list = list(my_dict.values())
+
+
+print(keys_list)
