42b48ba11554190af83f174488e3a86335c761e1
Wondwosen <revatoel@gmail.com>
2024-04-11 02:22:45 +0300

    done- tired af

--- File Handling/file.py
+++ File Handling/file.py
@@ -1,4 +1,6 @@
 file_path = "C:\\Users\\PC\\Documents\\python-lessons\\File Handling\\sample_data.txt"
 
+
+# don't forget to add double backslashes - cuz python treats backslashes as a escape character
 reader = open(file_path, "r")
 print(reader.read())
