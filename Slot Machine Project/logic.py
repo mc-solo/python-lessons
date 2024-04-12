2228f1ee9492f943fa1e25f5c6996a05cbc3f7a1
Wondwosen <revatoel@gmail.com>
2024-04-12 21:40:47 +0300

    issued spin logic implementaion

--- Slot Machine Project/logic.py
+++ Slot Machine Project/logic.py
@@ -10,10 +10,10 @@ COLS = 3
 symbol_count = {"A": 2, "B": 4, "C": 6, "D": 8}
 
 
-def get_slot_machine_spin():
-    all_symbols = []
-    for symbol, count in symbol_count.items():
-        all_symbols.extend([symbol] * count)
+# def get_slot_machine_spin():
+#     all_symbols = []
+#     for symbol, count in symbol_count.items():
+#         all_symbols.extend([symbol] * count)
 
 
 # Get amount from the user
