bb8a9e185f9f439acc944c419c9f0c7009937eda
Wondwosen <revatoel@gmail.com>
2024-04-13 18:19:53 +0300

    the get balance method

--- OOP/bank_account.py
+++ OOP/bank_account.py
@@ -2,3 +2,6 @@ class BankAccount:
     def __init__(self, initialBalance, accName):
         self.balance = initialBalance
         self.name = accName
+
+    def get_balance(self):
+        print(f"\n{self.name} has ${self.balance:.2f} in the bank account")
--- OOP/oop_project.py
+++ OOP/oop_project.py
@@ -1,9 +1,10 @@
 from bank_account import *
 
 Wondwosen = BankAccount(10000, "Mc_solo")
+Wondwosen.get_balance()
 # print(Wondwosen.name)
 # print(Wondwosen.balance)
 
 
-mule = BankAccount(3990, "Nani")
+# mule = BankAccount(3990, "Nani")
 # print(f"{mule.name} has ${mule.balance:.2f} in her accout")
