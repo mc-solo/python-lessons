042f7bfa47e8c05e4e0417989fdb6ffe787e70db
Wondwosen <revatoel@gmail.com>
2024-04-13 18:25:48 +0300

    the deposit method

--- OOP/bank_account.py
+++ OOP/bank_account.py
@@ -5,3 +5,7 @@ class BankAccount:
 
     def get_balance(self):
         print(f"\n{self.name} has ${self.balance:.2f} in the bank account")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print(f"Deposit completed. Account {self.name} has ${self.amount:.2f}")
