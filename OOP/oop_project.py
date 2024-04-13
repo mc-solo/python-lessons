88191d24ac142c3f0a4b001a13465644051f52f0
Wondwosen <revatoel@gmail.com>
2024-04-13 18:10:26 +0300

    __init__ method

--- OOP/oop_project.py
+++ OOP/oop_project.py
@@ -3,3 +3,7 @@ from bank_account import *
 Wondwosen = BankAccount(10000, "Mc_solo")
 print(Wondwosen.name)
 print(Wondwosen.balance)
+
+
+mule = BankAccount(3990, "Nani")
+print(f"{mule.name} has {mule.balance} in her accout")
