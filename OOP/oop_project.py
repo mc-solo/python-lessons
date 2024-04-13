4154a838b0a0df3ff47c13b2487469598cf2f43a
Wondwosen <revatoel@gmail.com>
2024-04-13 18:16:40 +0300

    additional methods for the super class

--- OOP/oop_project.py
+++ OOP/oop_project.py
@@ -1,9 +1,9 @@
 from bank_account import *
 
 Wondwosen = BankAccount(10000, "Mc_solo")
-print(Wondwosen.name)
-print(Wondwosen.balance)
+# print(Wondwosen.name)
+# print(Wondwosen.balance)
 
 
 mule = BankAccount(3990, "Nani")
-print(f"{mule.name} has {mule.balance} in her accout")
+# print(f"{mule.name} has ${mule.balance:.2f} in her accout")
